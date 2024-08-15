dcfe1bf1fdb7ad68bd66328dd46c83c6dd3dc1f1
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-08-15 19:30:56 +0530

    15-08

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -18,6 +18,31 @@ export async function GET(request: Request){
         }
 
         const result = UsernameQuerySchema.safeParse(queryParam)
+        if(!result.success) {
+            const usernameErrors = result.error.format().username?._errors || []
+            return Response.json({
+                success: false,
+                message: usernameErrors?.length > 0
+                ? usernameErrors.join(', ')
+                : 'Invalid query parameters',
+            }, {status: 400})
+        }
+
+        const {username} = result.data
+
+        const existingVerifiedUser = await UserModel.findOne({username, isVerified: true})
+
+        if(existingVerifiedUser) {
+            return Response.json({
+                success: false,
+                message: 'Username is already taken',
+            }, {status: 400})
+        }
+
+        return Response.json({
+            success: true,
+            message: 'Username is unique',
+        }, {status: 400})
     } catch (error) {
         console.error("Error checking username", error)
         return Response.json(
