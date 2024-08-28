bf55a2892bb65fdfdf50843fccdd7eee971b0209
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-08-29 01:42:02 +0530

    29-08

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -21,6 +21,14 @@ export async function POST(req: Request){
         const stream = OpenAIStream(response);
         return new StreamingTextResponse(stream);
     } catch (error) {
-        
+        if(error instanceof OpenAI.APIError){
+            const {name, status, headers, message} = error
+            return NextResponse.json({
+                name, status, headers, message
+            }, {status})
+        } else {
+            console.error("An unexpected error occured", error)
+            throw error
+        }
     }
 }
