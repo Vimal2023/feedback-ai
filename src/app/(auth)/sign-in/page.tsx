42b8b6d560133ab8727fca80a19ecd5f09288f76
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-09-05 19:48:48 +0530

    .

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,10 +3,13 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z  from "zod"
 import Link from "next/Link"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useDebounceValue } from 'usehooks-ts'
 import { useToast } from "@/components/ui/use-toast"
 import { useRouter } from "next/navigation"
+import { signUpSchema } from "@/schemas/signUpSchema"
+import axios, {AxiosError} from 'axios'
+import {ApiResponse } from "@/types/ApiResponse"
 
 
 const page = () => {
@@ -18,9 +21,33 @@ const page = () => {
     const debouncedUsername = userDebounceValue(username, 300)
     const { toast } = useToast()
     const router = useRouter();
-    const form = useForm({
-      
+    const form = useForm<z.infer<typeof signUpSchema>>({
+      resolver: zodResolver(signUpSchema),
+      defaultValues:{
+        username: '',
+        email: '',
+        password: ''
+      }
     })
+
+    useEffect(() => {
+      const checkUsernameUnique = async () => {
+        if(debouncedUsername) {
+          setIsCheckingUsername(true)
+          setusernameMessage('')
+          try {
+            const response = await axios.get(`/api/
+            check-username-unique?username=${debouncedUsername}
+            `)
+            setusernameMessage(response.data.message)
+          } catch (error) {
+            
+          }
+        }
+      }
+    }
+    , [debouncedUsername])
+    
 
 
   return (
