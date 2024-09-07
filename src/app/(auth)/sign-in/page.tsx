e17565e09b59ddba4bd6af03bb5b362f512dfa43
Vimal Anand <78069624+Vimal2023@users.noreply.github.com>
2024-09-07 23:32:34 +0530

    some work done on UI part

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -10,6 +10,15 @@ import { useRouter } from "next/navigation"
 import { signUpSchema } from "@/schemas/signUpSchema"
 import axios, {AxiosError} from 'axios'
 import {ApiResponse } from "@/types/ApiResponse"
+import { Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage, } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
 
 
 const page = () => {
@@ -41,18 +50,73 @@ const page = () => {
             `)
             setusernameMessage(response.data.message)
           } catch (error) {
-            
+            const axiosError = error as AxiosError<ApiResponse>;
+            setUsernameMessage(
+              axiosError.response?.data.message ?? "Error checking username"
+            )
+          } finally {
+            setIsCheckingUsername(false)
           }
         }
       }
-    }
-    , [debouncedUsername])
+      checkUsernameUnique()
+    }, [debouncedUsername])
     
-
-
+    const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+      setIsSubmitting(true)
+      try {
+        const response = await axios.post<ApiResponse>('/api/sign-up', data)
+        toast({
+          title: 'Success',
+          description: response.data.message
+        })
+        router.replace(`/verify/${username}`)
+        setIsSubmitting(false)
+      } catch (error) {
+        console.error("Error in signup of user", error)
+        const axiosError = error as AxiosError<ApiResponse>;
+        let errorMessage = axiosError.response?.data.message
+        toast({
+          title: "Signup failed",
+          description: errorMessage,
+          variant: "destructive"
+        })
+        setIsSubmitting(false)
+      }
+    }
   return (
-    <div>
-      
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Message
+          </h1>
+          <p className="mb-4">Sign up to start your anonymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="username" 
+                {...field}
+                onChange={(e) => {
+                  field.onChange(e)
+                  setUsername(e.target.value)
+                }} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </form>
+        </Form>
+
+      </div>
     </div>
   )
 }
